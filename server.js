7104593b886a5cdf2847d391fa13e80292634186
Brilian <briliankrisna27@gmail.com>
2024-06-08 13:18:45 +0700

    Initial NodeJS deployment

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Test');
+    response.end();
+}).listen(8080);
