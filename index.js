b6c1e2dbfd4b1d7b7fe3f085e77bdaa68693f410
Brilian <briliankrisna27@gmail.com>
2024-07-15 14:29:44 +0700

    Another profile card testing

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const title = document.getElementById("title");
 const bg = document.getElementsByTagName("body");
-const h1 = document.getElementsByTagName("h1");
+const h1 = document.getElementsByClassName("world")
+h1.style = "color: black;";
 
 function darkTheme() {
-    h1.style.color = 'black';
+    window.alert("Test")
 };
